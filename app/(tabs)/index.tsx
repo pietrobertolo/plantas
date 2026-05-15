f20f9298a90c7a715177b7e3b562f2ff8d316cac
Pietro Sandri <pietro.sandri@preve.g12.br>
2026-05-15 17:04:23 -0300

    att

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,43 @@
 import { Text, View, StyleSheet } from "react-native";
 
+import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
 const PlaceholderImage = require('@/assets/images/images.jpg');
 
-export default function Index() 
+export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled){
+      setSelectedImage(result.assets[0].uri);
+    } else{
+      alert('You did not select any image.');
+    }
+  } 
+
 {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage}/>
+        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
-
+      <View style={styles.footerContainer}>
+         <Button theme ="primary" label="choose a photo"/>
+         <Button label="Use this photo"/>   
+      </View>
     </View>
   );
 }
-
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -31,5 +53,9 @@ const styles = StyleSheet.create({
     width: 320,
     height: 440,
     borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
