707897b9d6146e6322f5dfc3e0bf34d0d725f972
Pietro Sandri <pietro.sandri@preve.g12.br>
2026-06-12 16:21:23 -0300

    adicionando sticker

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet } from "react-native";
+import { ImageSourcePropType, View, StyleSheet } from "react-native";
 
 import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
@@ -6,12 +6,17 @@ import * as ImagePicker from 'expo-image-picker';
 import { useState } from 'react';
 import IconButton from "@/components/IconButton";
 import CircleButton from "@/components/CircleButton";
+import EmojiPicker from "@/components/EmojiPicker";
+import EmojiList from "@/components/EmojiList";
+import EmojiSticker from "@/components/EmojiSticker";
 
 const PlaceholderImage = require('@/assets/images/images.jpg');
 
 export default function Index() {
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [pickedEmoji, setPickedEmoji] = useState<ImageSourcePropType | undefined>(undefined);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -33,7 +38,11 @@ export default function Index() {
   };
 
   const onAddSticker = () => {
-    // we will implement this later
+    setIsModalVisible(true);
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
   };
 
   const onSaveImageAsync = async () => {
@@ -45,6 +54,7 @@ export default function Index() {
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
+        {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji}/>}
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
@@ -60,6 +70,9 @@ export default function Index() {
          <Button label="Use this photo" onPress={() => setShowAppOptions(true)}/>   
       </View>
        )}
+       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose}/>
+       </EmojiPicker>
     </View>
   );
 }
---
+++ components/EmojiList.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import { ImageSourcePropType, StyleSheet, FlatList, Platform, Pressable } from 'react-native';
+import { Image } from 'expo-image';
+
+type Props = {
+  onSelect: (image: ImageSourcePropType) => void;
+  onCloseModal: () => void;
+};
+
+export default function EmojiList({ onSelect, onCloseModal }: Props) {
+  const [emoji] = useState<ImageSourcePropType[]>([
+    require('../assets/images/9440160.png'),
+    require('../assets/images/9440163.png'),
+    require('../assets/images/9440173.png'),
+    require('../assets/images/9440192.png'),
+    require('../assets/images/12114332.png'),
+    require('../assets/images/jardinagem.png'),
+  ]);
+
+  return (
+    <FlatList
+      horizontal
+      showsHorizontalScrollIndicator={Platform.OS === 'web'}
+      data={emoji}
+      contentContainerStyle={styles.listContainer}
+      renderItem={({ item, index }) => (
+        <Pressable
+          onPress={() => {
+            onSelect(item);
+            onCloseModal();
+          }}
+        >
+          <Image source={item} key={index} style={styles.image} />
+        </Pressable>
+      )}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  listContainer: {
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginRight: 20,
+  },
+});
